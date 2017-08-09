c8f786e91da8c6f8b3a9bcdeb95366de86f463dd
David Atchley <david.m.atchley@gmail.com>
2017-08-09 17:09:44 -0500

    Addition of content blocks to playbook page, gradient cleanup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 /**
  * index.js
  */
+import qs from 'query-string';
 
 if (typeof console === 'object') {
     console.log('\n' +
-        '  |\\      _,,,,--,,_       \n' + 
-        '  /,`.-\'`\'    -,  \\-;,   \n' +  
-        ' |,4-  ) ),,__ ,\\ (  ;;    [t] tandem.ly\n' + 
-        ' ---\'\'(.\'--\'  `-\'`.)`  \n' + 
+        '  |\\      _,,,,--,,_       \n' +
+        '  /,`.-\'`\'    -,  \\-;,   \n' +
+        ' |,4-  ) ),,__ ,\\ (  ;;    [t] tandem.ly\n' +
+        ' ---\'\'(.\'--\'  `-\'`.)`  \n' +
         '\n' +
         'Hey there!  We\'re glad you\'re curious about our code.\n' +
         'We\'d love to chat with you about what you\'re working on\n' +
@@ -27,43 +28,36 @@ toggle.addEventListener('click', () => {
   header.classList.toggle('open');
 });
 
-// const hero = document.querySelector('.hero .container_full');
-// const fixed_cta = document.querySelector('.fixed-scroll-header');
-// const logo = document.querySelector('.logo');
+const form = document.querySelector('form[name="hireus"]');
+const connect_endpoint = "https://calendly.com/tandemly-core-team/discovery-chat";
+
+const connect = (name, email) => {
+    const params = qs.stringify({ name, email });
+    const url = [connect_endpoint, '?', params].join('');
+    console.log(`going to url: ${url}`);
+    window.location.href = url;
+}
+
+if (form) {
+    form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        const name = form.querySelector('input[name="name"]');
+        const email = form.querySelector('input[name="email"]');
+        connect(name, email);
+    });
+}
 
 // const pos = hero.getBoundingClientRect().bottom;
 const pos = (hero && hero.offsetHeight) || 150;
 const scroll = window.requestAnimationFrame;
 console.log(`pos = ${pos}`)
 
-// const removeClass = (el, className) => {
-//     if (el.classList)
-//         el.classList.remove(className);
-//     else
-//         el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-// }
-
-// const addClass = (el, className) => {
-//     if (el.classList)
-//         el.classList.add(className);
-//     else
-//         el.className += ' ' + className;
-// }
-
 const loop = () => {
     if (window.pageYOffset >= pos) { // && /slide-up/.test(fixed_cta.className)) {
-        // console.log('show background');
         header.classList.add('affixed');
-    //     removeClass(fixed_cta, 'slide-up');
-    //     addClass(fixed_cta, 'slide-down');
-    //     addClass(logo, 'fixed-logo');
     }
     if (window.pageYOffset < pos ) { // && /slide-down/.test(fixed_cta.className)) {
-        // console.log('hide background');
         header.classList.remove('affixed');
-    //     removeClass(fixed_cta, 'slide-down');
-    //     addClass(fixed_cta, 'slide-up');
-    //     removeClass(logo, 'fixed-logo');
     }
     scroll(loop);
 }
