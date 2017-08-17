093aab0db41b9b8380cc22d255fe65af42f08747
Dave Atchley <david.m.atchley@gmail.com>
2017-08-17 02:55:32 -0500

    Develop (initial full site) (#1)
    * Updates for main page content, nav changes
    
    * updates for firebase, testing
    
    * travis build changes
    
    * travis build changes
    
    * travis buid fixes
    
    * build changes
    
    * Updates for styling, additional pages.
    
    * fixes for minifying in builds
    
    * fixed current link on home page
    
    * Updates to career page
    
    * fix spacing on tags for mobile
    
    * setup for narrow columns for content on pages.
    
    * styling changes, additional about page content
    
    * changes to page headers and text leaders, plus secondary button styling.
    
    * styling and layout changes
    
    * Addition of content blocks to playbook page, gradient cleanup
    
    * small content changes looking at voice and tone.
    
    * Cleaning up content, basic styling.
    
    * layout + styling fixes
    
    * styling + layout fixes and updates
    
    * styling/ colors
    
    * Styling changes for career role cards, adding people icons and formatting.
    
    * clean up flexbox version of skeleton grid, images.
    
    * added opacity to backing circle for role avatars.
    
    * images, styling changes for playbook page.
    
    * additional img for about page, styling changes
    
    * addition of a simple flexbox grid (my own crappy implementation) and some styling changes for the top section of pages other than home.
    
    * final styling changes, addition of simple flex-grid, iconography, illustrations.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,89 @@
+/* global ga */
 /**
  * index.js
  */
+import qs from 'query-string';
 
 if (typeof console === 'object') {
     console.log('\n' +
-        '  |\\      _,,,,--,,_       Hey there!  We\'re glad you\'re curious about our code.\n' +
-        '  /,`.-\'`\'    -,  \\-;,     We\'d love to chat with you about what you\'re working on\n' +
-        ' |,4-  ) ),,__ ,\\ (  ;;    and see how you could help us improve!\n' +
-        ' ---\'\'(.\'--\'  `-\'`.)`               -- hireme@tandem.ly\n' +
+        '  |\\      _,,,,--,,_       \n' +
+        '  /,`.-\'`\'    -,  \\-;,   \n' +
+        ' |,4-  ) ),,__ ,\\ (  ;;    [t] tandem.ly\n' +
+        ' ---\'\'(.\'--\'  `-\'`.)`  \n' +
+        '\n' +
+        'Hey there!  We\'re glad you\'re curious about our code.\n' +
+        'We\'d love to chat with you about what you\'re working on\n' +
+        'and see how you could help us improve!\n' +
+        '                                      -- hireme@tandem.ly\n' +
         '\n'
     );
 }
+var toggle = document.querySelector('.header__nav-button');
+var nav = document.querySelector('nav.global-nav');
+const header = document.querySelector('#header');
+const hero = document.querySelector('.hero .container_full') || document.querySelector('.section.first .container_full');
+const hero_button = hero && hero.querySelector('.cta .button');
+const input = document.querySelector('.hireus-form-container input[name="name"]');
 
-const hero = document.querySelector('.hero .container_full');
-const fixed_cta = document.querySelector('.fixed-scroll-header');
-const pos = hero.offsetHeight;
+if (hero_button) {
+    hero_button.addEventListener('click', (ev) => {
+        input && input.focus();
+        ev.preventDefault();
+    })
+}
+
+
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('open');
+  nav.classList.toggle('open');
+  header.classList.toggle('open');
+});
+
+const form = document.querySelector('form[name="hireus"]');
+const connect_endpoint = "https://calendly.com/tandemly-core-team/discovery-chat";
+const nav_cta = document.querySelector('header nav li.cta a');
+
+const connect = (name, email) => {
+    const params = qs.stringify({ name, email });
+    const url = [connect_endpoint, '?', params].join('');
+    galink(url, 'CTA Form Click', 'cta-form-click');
+}
+
+const galink = (url, eventName, label) => {
+    ga('send', 'event', eventName, 'click', {
+        hitCallback: () => {
+            window.location.href = url;
+        }
+    });
+}
+
+if (nav_cta) {
+    nav_cta.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        galink(ev.target.href, 'CTA Nav Click', 'cta-nav-click')
+    });
+}
+
+if (form) {
+    form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        const name = form.querySelector('input[name="name"]').value;
+        const email = form.querySelector('input[name="email"]').value;
+        connect(name, email);
+    });
+}
+
+// const pos = hero.getBoundingClientRect().bottom;
+const pos = (hero && hero.offsetHeight) || 150;
 const scroll = window.requestAnimationFrame;
 
-const removeClass = (el, className) => {
-    if (el.classList)
-        el.classList.remove(className);
-    else
-        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-}
-
-const addClass = (el, className) => {
-    if (el.classList)
-        el.classList.add(className);
-    else
-        el.className += ' ' + className;
-}
-
 const loop = () => {
-    if (window.pageYOffset >= pos && /slide-up/.test(fixed_cta.className)) {
-        removeClass(fixed_cta, 'slide-up');
-        addClass(fixed_cta, 'slide-down');
+    if (window.pageYOffset >= pos) { // && /slide-up/.test(fixed_cta.className)) {
+        header.classList.add('affixed');
     }
-    if (window.pageYOffset < pos && /slide-down/.test(fixed_cta.className)) {
-        removeClass(fixed_cta, 'slide-down');
-        addClass(fixed_cta, 'slide-up');
+    if (window.pageYOffset < pos ) { // && /slide-down/.test(fixed_cta.className)) {
+        header.classList.remove('affixed');
     }
     scroll(loop);
 }
